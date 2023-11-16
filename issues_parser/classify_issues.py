bf3d90315bdfb74115034461c4e3a0794c6ddbd9
Mohamed Ayman <mohamedayman15069@aucegypt.edu>
2023-11-15 17:04:57 -0700

    Added the RUN_ChatGPT function

--- issues_parser/classify_issues.py
+++ issues_parser/classify_issues.py
@@ -5,6 +5,22 @@ import argparse
 import re
 import inspect
 
+
+import datetime
+import json
+import os
+import sys
+
+from csv import DictReader
+from tqdm import tqdm
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)
+
+regex = re.compile(r"\d+\.\s")
+
 def setup():
     client = OpenAI(
         api_key=config("SECRET_KEY")
@@ -13,6 +29,27 @@ def setup():
     with open(config("PROMPT_LOC"), 'r') as f:
         initial_prompt = f.read()
     return client, initial_prompt
+
+@retry(wait=wait_random_exponential(min=5, max=60), stop=stop_after_attempt(10))
+def run_chatgpt(model, temp, meta_prompt, max_tokens):
+    """
+    Run the ChatGPT model on the input prompt.
+    """
+    # Define the parameters for the text generation
+    completions = OpenAI.Completion.create(
+        engine=model,
+        prompt=meta_prompt,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        temperature=temp,
+    )
+    gen_prompt = completions.choices[0].text.strip().lower()
+    # Print the generated text
+    print("The meta prompt is --> ", meta_prompt)
+    print("LLM output is --> ", gen_prompt)
+    return gen_prompt
+
 
 def generate_issue_prompts(csv_file: str, no_of_samples: int):
     data = pd.read_csv(csv_file)
@@ -33,17 +70,25 @@ def generate_issue_prompts(csv_file: str, no_of_samples: int):
 
 def main():
     parser = argparse.ArgumentParser(allow_abbrev=True)
-
+    client, initial_prompt = setup()
     parser.add_argument("csv_files", nargs="+", help="Locations of the csv files of issues.")
     parser.add_argument("-n", "--no_of_samples", type=int, default=100, metavar="", help="Number of samples from each csv file. Default to 100")
     args = parser.parse_args()
 
-    client, initial_prompt = setup()
 
     issue_prompts = []
     for csv_file in args.csv_files:
         print("Generating prompt for file: " + csv_file)
         issue_prompts += generate_issue_prompts(csv_file, args.no_of_samples)
+    gpt_name = "gpt-3.5-turbo-1106"
+    for issue_prompt in tqdm(issue_prompts):
+        full_prompt = initial_prompt + ' ' +issue_prompt
+        run_chatgpt(
+            model=gpt_name,
+            temp=0,
+            meta_prompt=full_prompt,
+            max_tokens=16000,
+        )
 
 if __name__ == "__main__":
     try:
