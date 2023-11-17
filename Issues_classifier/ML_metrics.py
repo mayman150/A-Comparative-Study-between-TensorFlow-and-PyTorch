0bfa666300f6ebf21cea1c6f7b18760920e1cf0c
Mohamed Ayman <mohamedayman15069@aucegypt.edu>
2023-11-17 00:30:06 -0700

    Tested the implemented functions

--- Issues_classifier/ML_metrics.py
+++ Issues_classifier/ML_metrics.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import pandas as pd 
 import torch
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 #accuracy
@@ -56,3 +57,31 @@ def f1_score(y_pred, y_true):
     f1 = 2 * (p * r) / (p + r + 1e-9)
     return f1
 
+
+
+
+# #test the functions
+# def test():
+#     # True labels
+#     y_true = torch.tensor([1, 0, 1, 1, 0, 1, 0, 1, 0, 0])
+
+#     # Predicted labels
+#     y_pred = torch.tensor([1, 0, 1, 1, 1, 0, 1, 0, 0, 1])
+#     print("Accuracy: ", accuracy(y_pred, y_true))
+#     print("Precision: ", precision(y_pred, y_true))
+#     print("Recall: ", recall(y_pred, y_true))
+#     print("F1 Score: ", f1_score(y_pred, y_true))
+
+#     # Expected values
+#     expected_accuracy = 0.5
+#     expected_precision = 0.5
+#     expected_recall = 0.6000000238418579
+#     expected_f1 = 0.5454545548106848
+#     assert(accuracy(y_pred, y_true) == expected_accuracy)
+#     assert(precision(y_pred, y_true) == expected_precision)
+#     assert(recall(y_pred, y_true) == expected_recall)
+#     assert(f1_score(y_pred, y_true) == expected_f1)
+#     print("Test passed")
+
+# if __name__ == "__main__":
+#     test()
