db0684d5a17a690bc5c4927a538a22d1fcda5953
Mohamed <mohamedayman15069@aucegypt.edu>
2023-11-20 15:29:00 -0700

    organizing evolution over bug folder

---
+++ Evolution_Over_Bugs_Analysis/Issues_classifier/ML_Model_Training/Neural_Network_Implementation/ML_metrics.py
@@ -0,0 +1,87 @@
+import numpy as np 
+import pandas as pd 
+import torch
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+
+#accuracy
+def accuracy(y_pred, y_true):
+    '''
+    Input: y_pred, y_true
+    Output: Accuracy
+    '''
+    return np.mean((y_pred > 0.5).data == y_true.data)
+
+
+#Precision
+def precision(y_pred, y_true):
+    '''
+    Input: y_pred, y_true
+    Output: Precision
+    '''
+    y_pred_binary = torch.round(y_pred)
+    true_positives = torch.sum((y_true == 1) & (y_pred_binary == 1)).float()
+    false_positives = torch.sum((y_true == 0) & (y_pred_binary == 1)).float()
+    # Avoid division by zero
+    precision = true_positives / (true_positives + false_positives + 1e-9)
+    return precision.item()  # Convert to Python float
+
+
+#Recall
+def recall(y_pred, y_true):
+    '''
+    Input: y_pred, y_true
+    Output: Recall
+    '''
+    # Convert predictions to binary (1 if probability >= 0.5, else 0)
+    y_pred_binary = torch.round(y_pred)
+
+    # Calculate true positives (correctly predicted positives) and false negatives
+    true_positives = torch.sum((y_true == 1) & (y_pred_binary == 1)).float()
+    false_negatives = torch.sum((y_true == 1) & (y_pred_binary == 0)).float()
+    # Avoid division by zero
+    recall = true_positives / (true_positives + false_negatives + 1e-9)
+    return recall.item()  # Convert to Python float
+
+#F1 Score
+def f1_score(y_pred, y_true):
+    '''
+    Input: y_pred, y_true
+    Output: F1 Score
+    '''
+    # Calculate precision and recall using previously defined functions
+    p = precision(y_pred, y_true)
+    r = recall(y_pred, y_true)
+
+    # Avoid division by zero
+    f1 = 2 * (p * r) / (p + r + 1e-9)
+    return f1
+
+
+
+
+# #test the functions
+# def test():
+#     # True labels
+#     y_true = torch.tensor([1, 0, 1, 1, 0, 1, 0, 1, 0, 0])
+
+#     # Predicted labels
+#     y_pred = torch.tensor([1, 0, 1, 1, 1, 0, 1, 0, 0, 1])
+#     print("Accuracy: ", accuracy(y_pred, y_true))
+#     print("Precision: ", precision(y_pred, y_true))
+#     print("Recall: ", recall(y_pred, y_true))
+#     print("F1 Score: ", f1_score(y_pred, y_true))
+
+#     # Expected values
+#     expected_accuracy = 0.5
+#     expected_precision = 0.5
+#     expected_recall = 0.6000000238418579
+#     expected_f1 = 0.5454545548106848
+#     assert(accuracy(y_pred, y_true) == expected_accuracy)
+#     assert(precision(y_pred, y_true) == expected_precision)
+#     assert(recall(y_pred, y_true) == expected_recall)
+#     assert(f1_score(y_pred, y_true) == expected_f1)
+#     print("Test passed")
+
+# if __name__ == "__main__":
+#     test()
---
+++ Evolution_Over_Bugs_Analysis/Issues_classifier/ML_Model_Training/Neural_Network_Implementation/data_loader.py
@@ -0,0 +1,102 @@
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+from torch.utils.data import Dataset, DataLoader
+import ast
+import numpy as np
+import torch
+import argparse
+import re
+
+def remove_extra_commas(input_string):
+    # Use regular expression to replace multiple commas with a single comma
+    cleaned_string = re.sub(',+', ',', input_string)
+    return cleaned_string
+
+
+def modify(input_string):
+    input_string = input_string.replace('\n', '').replace(' ', ',')
+    input_string = remove_extra_commas(input_string)
+    if(input_string[1] == ','):
+        input_string = input_string[0] + input_string[2:]
+    input_string = ast.literal_eval(input_string.replace(' ',''))
+    return input_string
+
+
+def preprocess_data(args):
+    data = pd.read_csv(args.DataPath)
+    if args.Training_Data == 'Training_Data_with_Title_BERT':
+        col = 'Issue Title'
+    else:
+        col = "BERT Embedding"
+        
+    X = data[col].apply(lambda x: modify(x))
+    X = np.asarray(X.values.tolist(), dtype=np.float32)
+    #Given data['Is Bug']make them 0 and 1
+    data['Is Bug'] = data['Is Bug'].apply(lambda x: 1 if x == True else 0)
+    #get it as numpy array
+    y = np.asarray(data['Is Bug'], dtype=np.uint8)
+
+    return X,y
+
+
+class CustomDataset(Dataset):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+
+def load_data(args:argparse.Namespace, n_splits=5, batch_size=32):
+    '''
+    Load the data from the csv file and return DataLoader instances for cross-validation
+    '''
+    X, y = preprocess_data(args)
+    skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+
+    # Convert to PyTorch tensors
+    X_tensor = torch.tensor(X).float()
+    y_tensor = torch.tensor(y).float()
+
+    loaders = []
+
+    for train_index, test_index in skf.split(X, y):
+        X_train, X_test = X_tensor[train_index], X_tensor[test_index]
+        y_train, y_test = y_tensor[train_index], y_tensor[test_index]
+
+        # Create custom datasets
+        train_dataset = CustomDataset(X_train, y_train)
+        test_dataset = CustomDataset(X_test, y_test)
+
+        # Create DataLoader instances
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+        loaders.append((train_loader, test_loader))
+
+    return loaders, X_train.shape[1]
+
+
+
+# def test():
+
+#     # Example usage:
+#     cross_validation_loaders = load_data(n_splits=5, batch_size=32)
+
+#     # Iterate over the cross-validation loaders
+#     for train_loader, test_loader in cross_validation_loaders:
+#         for inputs, labels in train_loader:
+#             # Your training logic here
+#             pass
+
+#         for inputs, labels in test_loader:
+#             # Your testing/evaluation logic here
+#             pass
+
+
+# if __name__ == '__main__':
+#     test()
