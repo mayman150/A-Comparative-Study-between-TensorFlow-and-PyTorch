cea69807f31b2ffff8f649c0c3b358135aa48eb9
Mohamed <mohamedayman15069@aucegypt.edu>
2023-12-01 00:28:30 -0700

    added issues

--- Evolution_Over_Bugs/Issues_parser/scraper.py
+++ Evolution_Over_Bugs/Issues_parser/scraper.py
@@ -6,19 +6,19 @@ from utils import get_issues, write_issues_to_csv
 
 def __main__():
     # authenticate with the token
-    ACCESS_TOKEN = "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"
+    ACCESS_TOKEN = "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"
     g = Github(ACCESS_TOKEN)
     # set the date two years ago
 
     #Pytorch
-    issues = get_issues(g, 'pytorch/pytorch', state='closed')
+    issues = get_issues(g, 'pytorch/pytorch', state='all')
     # write issues to a CSV file
-    write_issues_to_csv(g, issues, 'PyTorch_issues.csv', ['Issue Number', 'Issue Title', 'Issue Body', 'Time created', 'Time closed', 'Number of Assignees', 'Number of Comments', 'Tags'])
+    write_issues_to_csv(g, issues, 'PyTorch_issues_final2.csv', ['Issue Number', 'Issue Title',  'Time created', 'Time closed', 'Number of Assignees', 'Number of Comments', 'Tags'])
 
     #Tensorflow
-    issues = get_issues(g, 'tensorflow/tensorflow', state='closed')
+    issues = get_issues(g, 'tensorflow/tensorflow', state='all')
     # write issues to a CSV file
-    write_issues_to_csv(g,issues, 'Tensorflow_issues.csv', ['Issue Number', 'Issue Title', 'Issue Body', 'Time created','Time closed' , 'Number of Assignees', 'Number of Comments', 'Tags'])
+    write_issues_to_csv(g,issues, 'Tensorflow_issues_final2.csv', ['Issue Number', 'Issue Title',  'Time created','Time closed' ,  'Number of Assignees', 'Number of Comments', 'Tags'])
 
 
 if __name__ == "__main__":
